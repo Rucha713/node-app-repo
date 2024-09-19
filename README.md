b529f96b24b515f72f0c764463980f5b57e6d159
Rucha713 <155422655+Rucha713@users.noreply.github.com>
2024-09-19 11:09:55 +0530

    Assign

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # node-app-repo
 This is Node APP Repository
+
+# Project build steps
+
+1. Clone the repository - <repo-url>
+
+2. npm install or npm update
+
+ 
+
+# Author
+Rucha Tungatkar
+
+Mobile: 7020537417
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('my first app');
+const os = require('os');
+
+ 
+console.log('My first app');
+
+console.log(`File Name: ${__filename}`);
+
+console.log(`Directory Name: ${__dirname}`);
+
+ 
+
+console.log("=============== Built in Module - OS =======================");
+
+console.log(`Arch: ${os.arch()}`);
+
